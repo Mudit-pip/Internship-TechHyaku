f91573c20efe88a14e548b86e8ff147e60b302df
ccell <70972592+Mudit-pip@users.noreply.github.com>
2025-07-25 00:56:34 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import Header from './components/Header.jsx'
-import Courses from './components/Courses.jsx'
-import Collaboration from './components/Collaboration.jsx'
-import Home from './components/Home.jsx'
-import Video from './components/Video.jsx'
-import WhyUs from './components/whyUs.jsx'
-import AboutSection from './components/About.jsx'
-import AchievementsSection from './components/Achievements.jsx'
-import KitsSection from './components/KitSection.jsx'
-import Footer from './components/Footer.jsx'
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Courses from "./components/Courses.jsx";
+import Collaboration from "./components/Collaboration.jsx";
+import Home from "./components/Home.jsx";
+import Video from "./components/Video.jsx";
+import WhyUs from "./components/WhyUs.jsx";
+import AboutSection from "./components/About.jsx";
+import AchievementsSection from "./components/Achievements.jsx";
+import KitsSection from "./components/KitSection.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header/>
-      <Home/>
-      <AboutSection/>
-      <WhyUs/>
-      <Courses/>
-      <KitsSection/>
-      <Video/>
-      <AchievementsSection/>
-      <Collaboration/>
-      <Footer/>
-      
+      <Header />
+      <Home />
+      <AboutSection />
+      <WhyUs />
+      <Courses />
+      <KitsSection />
+      <Video />
+      <AchievementsSection />
+      <Collaboration />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
